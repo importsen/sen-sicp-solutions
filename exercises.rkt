191ac6744b18d4893320c68f72b65745dd2d3a3d
sen <senecayung@gmail.com>
2023-10-18 20:28:53 -0400

    update notes

--- exercises.rkt
+++ exercises.rkt
@@ -211,30 +211,24 @@
 ;
 ;=============================================================================
 ;1.8
-;TODO will figure this out later
 
 (define (square x)(* x x))
-
-(define (cube-root-iter guess previous-guess x)
-  (if (good-enough? guess previous-guess)
-      guess
-      (cube-root-iter (improve guess x)
-                guess
-                x)))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (improve guess x)
-  (/ (+ (/ x (* guess guess))
-        (* 2 guess)) 
-  3))
-
-(define (good-enough? previous-guess guess)
-  (< (abs (- guess previous-guess)) 0.001))
-
-(define (cube-root x)
-  (cube-root-iter 1.0 0 x))
+  (define (cube-root-iter guess previous-guess x)
+    (if (good-enough? guess previous-guess)
+        guess
+        (cube-root-iter (improve guess x)
+                  guess
+                  x)))
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (improve guess x)
+    (/ (+ (/ x (* guess guess))
+          (* 2 guess)) 
+    3))
+  (define (good-enough? previous-guess guess)
+    (< (abs (- guess previous-guess)) 0.001))
+  (define (cube-root x)
+    (cube-root-iter 1.0 0 x))
 
 (cube-root 25)
 
--- sicp-notes.rkt
+++ sicp-notes.rkt
@@ -1,15 +1,27 @@
 #lang sicp
 
-(define (square x) (* x x)) 
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
 
-(define (square-2 x)
-  (exp (double (log x))))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x) 
+  guess
+  (sqrt-iter (improve guess x) x)))
 
-(define (double x) (+ x x))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
 
-(double 3)
-(log 2)
-(square-2 2)
-(square 2)
-(exp 2)
-(exp (double (log 2)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+
+(define (sqrt x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess)))) 
+  (sqrt-iter 1.0))
