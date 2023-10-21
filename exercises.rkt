2e29146e58f13508a1e99c1c868d114c6b48103d
sen <senecayung@gmail.com>
2023-10-20 23:18:17 -0400

    1.9

--- exercises.rkt
+++ exercises.rkt
@@ -212,27 +212,38 @@
 ;=============================================================================
 ;1.8
 
-(define (square x)(* x x))
-  (define (cube-root-iter guess previous-guess x)
-    (if (good-enough? guess previous-guess)
-        guess
-        (cube-root-iter (improve guess x)
-                  guess
-                  x)))
-  (define (average x y)
-    (/ (+ x y) 2))
-  (define (improve guess x)
-    (/ (+ (/ x (* guess guess))
-          (* 2 guess)) 
-    3))
-  (define (good-enough? previous-guess guess)
-    (< (abs (- guess previous-guess)) 0.001))
-  (define (cube-root x)
-    (cube-root-iter 1.0 0 x))
+;(define (square x)(* x x))
+;  (define (cube-root-iter guess previous-guess x)
+;    (if (good-enough? guess previous-guess)
+;        guess
+;        (cube-root-iter (improve guess x)
+;                  guess
+;                  x)))
+;  (define (average x y)
+;    (/ (+ x y) 2))
+;  (define (improve guess x)
+;    (/ (+ (/ x (* guess guess))
+;          (* 2 guess)) 
+;    3))
+;  (define (good-enough? previous-guess guess)
+;    (< (abs (- guess previous-guess)) 0.001))
+;  (define (cube-root x)
+;    (cube-root-iter 1.0 0 x))
 
-(cube-root 25)
+;(cube-root 25)
 
 
 ;=============================================================================
+;1.9
 
+(define (+ a b)
+  (if (= a 0)
+    b
+    (inc (+ (dec a) b))))
 
+(define (+ a b)
+  (if (= a 0)
+    b
+    (+ (dec a) (inc b))))
+
+(+ 4 5)
